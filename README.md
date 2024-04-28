6c6167bb3c88d56fc301df2edaac452f990f19c3
Samy <samy.bouhamidi@efrei.net>
2024-04-28 19:20:08 +0200

    yams v1: necessary front + back

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
 # Yams
+
+## Introduction
+
+
+## Technologies utilisés
+
+**Front:** React TypeScript
+**Back:** Python
+
+## Installation du jeu
+
+### Clone du projet
+
+Déplacez-vous dans le dossier où vous voulez cloner le projet.
+Dans la barre de recherche écrivez :
+```
+git clone <lien>
+```
+
+Puis ouvrez le avec un éditeur de code compatible (VSCode, IntelliJ...).
+
+### Installation des dépendances
+
+Déplacez vous dans le dossier **yams** :
+```
+cd .\front\yams\
+```
+
+Puis installer les dépendances avec :
+```
+npm install
+```
+
+## Lancement du jeu
+
+### Lancement du site
+
+Dans le dossier **yams**, faîtes :
+```
+ npm run dev
+```
+
+Une **URL** s'affiche, c'est le lien du site.
+
+### Lancement du serveur
+
+Ouvrez un nouveau terminal sans fermer l'ancien.Allez dans le dossier **back** et faîtes :
+```
+python .\yams_api
+```
+
+Et après ça, allez sur le site et amusez vous bien. ☻
---
+++ back/yams_manager.py
@@ -0,0 +1,91 @@
+#from dices import Dices
+import random
+import copy
+from yams_utils import startChecking
+
+class YamsManager():
+    def __init__(self):
+        self.pseudoGenerator = ["boloss72"]
+        self.score = 0
+        self.combinations = {
+            "complexe":
+                [
+                    {'nom': "Brelan", 'score': -1},
+                    {'nom': "Carré", 'score': -1},
+                    {'nom': "Full", 'score': -1},
+                    {'nom': "Petite Suite", 'score': -1},
+                    {'nom': "Grande Suite", 'score': -1},
+                    {'nom': "Yams", 'score': -1},
+                    {'nom': "Chance", 'score': -1}
+                ],
+            "simple":
+                [
+                    {'nom': "Un", 'score': -1},
+                    {'nom': "Deux", 'score': -1},
+                    {'nom': "Trois", 'score': -1},
+                    {'nom': "Quatre", 'score': -1},
+                    {'nom': "Cinq", 'score': -1},
+                    {'nom': "Six", 'score': -1}
+                ]
+        }
+        self.initRound()
+
+
+    def initRound(self):
+        self.nbTurn = 3
+        self.diceKeep = []
+        self.dice = [0,0,0,0,0]
+
+
+    def rollDices(self):
+        for i in range(5):
+            self.dice[i] = random.randint(1, 6) if i not in self.diceKeep else self.dice[i]
+        self.nbTurn = self.nbTurn - 1
+        return self.nbTurn, self.dice
+    
+    def keepDices(self, dices):
+        self.diceKeep = dices
+
+    def calculateScores(self):
+        self.combinations = startChecking(self.dice, self.combinations)
+
+    def getCombinations(self):
+        return self.combinations
+
+    def setCombinations(self, combinations):
+        self.combinations = combinations
+
+    def getRandomPseudo(self):
+        i = random.randint(0, len(self.pseudoGenerator))
+        return self.pseudoGenerator[i]
+    
+    def getScore(self):
+        return self.score
+
+    def addScore(self, score):
+        self.score = self.score + score
+
+    def restartGame(self):
+        self.score = 0
+        self.combinations = {
+            "complexe":
+                [
+                    {'nom': "Brelan", 'score': -1},
+                    {'nom': "Carré", 'score': -1},
+                    {'nom': "Full", 'score': -1},
+                    {'nom': "Petite Suite", 'score': -1},
+                    {'nom': "Grande Suite", 'score': -1},
+                    {'nom': "Yams", 'score': -1},
+                    {'nom': "Chance", 'score': -1}
+                ],
+            "simple":
+                [
+                    {'nom': "Un", 'score': -1},
+                    {'nom': "Deux", 'score': -1},
+                    {'nom': "Trois", 'score': -1},
+                    {'nom': "Quatre", 'score': -1},
+                    {'nom': "Cinq", 'score': -1},
+                    {'nom': "Six", 'score': -1}
+                ]
+        }
+        self.initRound()
