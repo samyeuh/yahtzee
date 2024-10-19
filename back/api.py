eee876719e45637065a78122492f0f9647bdc5d7
Samy <samy.bouhamidi@efrei.net>
2024-10-19 19:45:22 +0200

    add ok code

--- back/api.py
+++ back/api.py
@@ -17,27 +17,33 @@ def keepDices():
     dice = request.json['dices']
     dice_indices = [int(d) for d in dice]
     yams.keepDices(dice_indices)
+    return jsonify({"message": "dice perfectly keeped"}), 200
 
 @app.route("/calculateScores")
 def calculateScores():
     yams.calculateScores()
+    return jsonify({"message": "score perfectly calculated"}), 200
     
 @app.route("/getCombinations", methods=["GET"])
 def getCombinations():
-    return jsonify(yams.getCombinations())
+    return jsonify(yams.getCombinations()), 200
+
 
 @app.route("/initRound", methods=["POST"])
 def initRound():
     yams.initRound()
+    return jsonify({"message": "round perfectly initied"}), 200
 
 @app.route("/setCombinations", methods=["POST"])
 def setCombinations():
     combinations = request.json["combinations"]
     yams.setCombinations(combinations)
+    return jsonify({"message": "combinations perfectly setted"}), 200
 
 @app.route('/restartGame', methods=["POST"])
 def restartGame():
     yams.restartGame()
+    return jsonify({"message": "game perfectly restarted"}), 200
 
 @app.route('/updateScore', methods=["POST"])
 def updateScore():
@@ -53,7 +59,7 @@ def updateScore():
 
 @app.route('/getTooltipDices', methods=["GET"])
 def getTooltipDices(): 
-    return jsonify(generateTooltipDices())
+    return jsonify(generateTooltipDices()), 200
     
 
 if __name__ == "__main__":
