dfaa80d349add2cf563f85f2fbee7445dabad0ee
samyeuh <samybouhamidi2@gmail.com>
2025-02-10 15:44:18 +0100

    Add fullscreen support for standalone mode in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,36 @@
 import './App.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, useLocation } from 'react-router-dom';
 import { GameplayProvider } from './context/GameplayContext/GameplayContext';
 import { Gameplay } from './page/Gameplay/Gameplay';
 import { Login } from './page/Login/Login';
+import { useEffect, useState } from 'react';
+
+export function useStandaloneMode() {
+  const [isStandalone, setIsStandalone] = useState(false);
+  const location = useLocation(); // Récupère la route actuelle
+
+  useEffect(() => {
+    // Fonction pour vérifier si l'application tourne en mode standalone
+    const checkStandalone = () => {
+      setIsStandalone(window.matchMedia("(display-mode: standalone)").matches);
+    };
+
+    checkStandalone(); // Vérifie au premier chargement
+
+    // Vérifie après chaque changement de route
+    window.addEventListener("resize", checkStandalone);
+
+    return () => {
+      window.removeEventListener("resize", checkStandalone);
+    };
+  }, [location]); // Se déclenche à chaque changement d'URL
+
+  return isStandalone;
+}
+
 
 function App() {
+  const isStandalone = useStandaloneMode();
   const router = createBrowserRouter([
     {
       children: [
@@ -26,7 +52,9 @@ function App() {
 
   return (
     <GameplayProvider>
-      <RouterProvider router={router} />
+      <div className={isStandalone ? "fullscreen-ios" : ""}>
+        <RouterProvider router={router} />
+      </div>
     </GameplayProvider>
   );
 }
