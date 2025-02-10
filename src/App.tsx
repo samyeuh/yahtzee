03ce1c71c9d472d8903bb45f39b540dd15181f53
samyeuh <samybouhamidi2@gmail.com>
2025-02-10 15:48:02 +0100

    remove apple home page edits

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,11 @@
 import './App.css';
-import { createBrowserRouter, RouterProvider, useLocation } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { GameplayProvider } from './context/GameplayContext/GameplayContext';
 import { Gameplay } from './page/Gameplay/Gameplay';
 import { Login } from './page/Login/Login';
-import { useEffect, useState } from 'react';
-
-export function useStandaloneMode() {
-  const [isStandalone, setIsStandalone] = useState(false);
-  const location = useLocation(); // Récupère la route actuelle
-
-  useEffect(() => {
-    // Fonction pour vérifier si l'application tourne en mode standalone
-    const checkStandalone = () => {
-      setIsStandalone(window.matchMedia("(display-mode: standalone)").matches);
-    };
-
-    checkStandalone(); // Vérifie au premier chargement
-
-    // Vérifie après chaque changement de route
-    window.addEventListener("resize", checkStandalone);
-
-    return () => {
-      window.removeEventListener("resize", checkStandalone);
-    };
-  }, [location]); // Se déclenche à chaque changement d'URL
-
-  return isStandalone;
-}
 
 
 function App() {
-  const isStandalone = useStandaloneMode();
   const router = createBrowserRouter([
     {
       children: [
@@ -52,9 +27,7 @@ function App() {
 
   return (
     <GameplayProvider>
-      <div className={isStandalone ? "fullscreen-ios" : ""}>
         <RouterProvider router={router} />
-      </div>
     </GameplayProvider>
   );
 }
